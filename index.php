9591b187cf0b93a737716ae6885cc2f9c1df7bf8
nickwu <j654ru8@gmail.com>
2016-11-08 10:27:27 +0800

    aspahalt web
    一頁式網頁

---
+++ index.php
@@ -0,0 +1,334 @@
+<!DOCTYPE html>
+<html lang="zh-Hant-TW">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="專營：柏油瀝青、道路鋪設、刨鋪路面、標線工程、停車場鋪設、路平專案，承接公家政府機關與私人公司企業的工程，免費估價！歡迎來電洽詢。我們是位於新北市板橋區的廠商(承包商)，三峽瀝青(中陸工業)工廠位置在桃園觀音區，擁有20幾年的施工經驗，值得您信賴！">
+    <meta name="author" content="">
+
+    <title>鋪設瀝青柏油AC廠商│奧斯福特營造</title>
+	<link rel="Shortcut Icon" type="image/x-icon" href="img/highway.png" />
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <!-- Plugin CSS -->
+    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+    <!-- Theme CSS -->
+	<link href="http://fonts.googleapis.com/earlyaccess/notosanstc.css" rel="stylesheet">
+    <link href="css/creative.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top">
+<?php include_once("analyticstracking.php") ?>
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand page-scroll" href="tel:0936069309">點擊速播&nbsp<i class="fa fa-phone fa-1x sr-contact"></i>0936069309</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a class="page-scroll" href="#about">專案</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#services">服務</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#portfolio">照片</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">聯絡</a>
+                    </li>
+					<li>
+                        <a class="page-scroll" href="#board">留言</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+    <header>
+        <div class="header-content">
+            <div class="header-content-inner">
+                <h1 id="homeHeading">鋪設瀝青柏油AC廠商│奧斯福特營造</h1>
+                <hr>
+                <p>專營：柏油瀝青、道路鋪設、刨鋪路面、標線工程、停車場鋪設、路平專案。<br>承接公家政府機關與私人公司企業的工程，免費估價！歡迎來電洽詢。</p>
+                <a href="#contact" class="btn btn-primary btn-xl page-scroll">馬上聯絡</a>
+            </div>
+        </div>
+    </header>
+
+    <section class="bg-primary" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading">專案</h2>
+                    <hr class="light">
+                    <p class="text-faded">
+					105年度大武崙工業區道路及人孔修繕工程<br>
+					105年度桃園市楊梅區轄管道路坑洞機動養護工程開口契約<br>
+					104年度桃園市政府工務局轄管道路坑洞機動養護工程開口契約(第一工區)<br>
+					103年度桃園縣編號道路坑洞機動養護工程開口契約(第一工區)<br>
+					103年度桃園縣編號道路坑洞機動養護工程開口契約(第二工區)
+					</p>
+                    <a href="#services" class="page-scroll btn btn-default btn-xl sr-button">更多</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="services">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">服務項目</h2>
+                    <hr class="primary">
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-road text-primary sr-icons"></i>
+                        <h3>道路工程</h3>
+                        <p class="text-muted">瀝青路面工程、路面刨除、標線、瀝青、柏油、舖柏油、鋪瀝青、柏油路面、柏油鋪面、鋪設柏油、瀝青鋪設、舖設瀝青、瀝青路面、熱拌瀝青、混凝土熱拌瀝青、瀝青買賣、瀝青道路、AC柏油路面、道路瀝青工程、路面修補、道路整修、瀝青混凝土鋪面</p>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-bars text-primary sr-icons"></i>
+                        <h3>標線工程</h3>
+                        <p class="text-muted">標誌、道路標線工程、路面標線、路面劃線、停車格標線</p>
+                    </div>
+                </div>
+                <!--div class="col-lg-4 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-newspaper-o text-primary sr-icons"></i>
+                        <h3>Up to Date</h3>
+                        <p class="text-muted">We update dependencies to keep things fresh.</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-heart text-primary sr-icons"></i>
+                        <h3>Made with Love</h3>
+                        <p class="text-muted">You have to make your websites with love these days!</p>
+                    </div>
+                </div-->
+            </div>
+        </div>
+    </section>
+
+    <section class="no-padding" id="portfolio">
+        <div class="container-fluid">
+            <div class="row no-gutter popup-gallery">
+                <div class="col-lg-4 col-sm-6">
+                    <a href="img/portfolio/1.jpg" class="portfolio-box">
+                        <img src="img/portfolio/1.jpg" alt="鋪設瀝青" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    桃園火車站
+                                </div>
+                                <div class="project-name">
+                                    中正路、復興路口
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="img/portfolio/2.jpg" class="portfolio-box">
+                        <img src="img/portfolio/2.jpg" alt="鋪設柏油" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    桃園
+                                </div>
+                                <div class="project-name">
+                                    中正東路
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="img/portfolio/3.jpg" class="portfolio-box">
+                        <img src="img/portfolio/3.jpg" alt="停車場鋪設" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    龜山
+                                </div>
+                                <div class="project-name">
+                                    忠義路
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="img/portfolio/4.jpg" class="portfolio-box">
+                        <img src="img/portfolio/4.jpg" alt="路面刨除" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    桃園巨蛋
+                                </div>
+                                <div class="project-name">
+                                    三民路
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="img/portfolio/5.jpg" class="portfolio-box">
+                        <img src="img/portfolio/5.jpg" alt="路平專案" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    楊梅
+                                </div>
+                                <div class="project-name">
+                                    青山一街
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="img/portfolio/6.jpg" class="portfolio-box">
+                        <img src="img/portfolio/6.jpg" alt="標線施工" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    桃園
+                                </div>
+                                <div class="project-name">
+                                    龍安街
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <aside class="bg-dark">
+        <div class="container text-center">
+            <div class="call-to-action">
+                <h2>更多工程照片</h2>
+                <a href="#" class="btn btn-default btn-xl sr-button">相簿</a>
+            </div>
+        </div>
+    </aside>
+
+    <section id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12  text-center">
+                    <h2 class="section-heading">聯絡我們</h2>
+                    <hr class="primary">
+                    <p>公司位於新北市板橋區，三峽瀝青(中陸工業)工廠位在桃園觀音區，擁有20幾年的施工經驗，提供專業完善工程品質、信譽良好，免費估價！歡迎來電洽詢。</p>
+                </div>
+                <div class="col-lg-3  text-center">
+                    <i class="fa fa-phone fa-3x sr-contact"></i>
+                    <p><a href="tel:0936069309">吳先生 0936-069-309</a></p>
+                </div>
+                <div class="col-lg-3 text-center">
+                    <i class="fa fa-envelope-o fa-3x sr-contact"></i>
+                    <p><a href="mailto:ac@asphalt.tw">AC@asphalt.tw</a></p>
+                </div>
+				<div class="col-lg-3 text-center">
+                    <i class="fa fa-building-o fa-3x sr-contact"></i>
+                    <p><a href="https://www.google.com.tw/maps/place/新北市板橋區縣民大道三段103巷18號">板橋區縣民大道三段103巷18號</a></p>
+                </div>
+				<div class="col-lg-3 text-center">
+                    <i class="fa fa-industry fa-3x sr-contact"></i>
+                    <p><a href="https://www.google.com.tw/maps/place/桃園市觀音區國建三路1號">桃園市觀音區國建三路1號</a></p>
+                </div>
+            </div>
+        </div>
+    </section>
+	
+	<section id="board">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12  text-center">
+                    <h2 class="section-heading">留言板</h2>
+					<hr class="primary">
+                </div>
+                <div class="col-lg-12  text-center">
+					<div id="disqus_thread"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+<script>
+
+/**
+*  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+*  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+
+var disqus_config = function () {
+this.page.url = 'http://www.asphalt.tw/';  // Replace PAGE_URL with your page's canonical URL variable
+this.page.identifier = 'index'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+};
+
+(function() { // DON'T EDIT BELOW THIS LINE
+var d = document, s = d.createElement('script');
+s.src = '//asphalt-tw.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="js/creative.min.js"></script>
+	
+	<!-- Disqus >
+	<script id="dsq-count-scr" src="//asphalt-tw.disqus.com/count.js" async></script-->
+</body>
+
+</html>
